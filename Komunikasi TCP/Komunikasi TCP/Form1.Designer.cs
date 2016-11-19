777e379913ca56f8d0124eba92bf31466b177e19
auliaiskandar <iskandar.bin.rusliahmad@gmail.com>
2016-11-20 05:14:23 +0700

    Praktikum Komunikasi TCP

---
+++ Komunikasi TCP/Komunikasi TCP/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Komunikasi_TCP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonLedOn = new System.Windows.Forms.Button();
+            this.buttonLedOff = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Address :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port :";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(81, 20);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIP.TabIndex = 2;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(81, 48);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPort.TabIndex = 3;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(198, 16);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 4;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonLedOn
+            // 
+            this.buttonLedOn.Location = new System.Drawing.Point(52, 105);
+            this.buttonLedOn.Name = "buttonLedOn";
+            this.buttonLedOn.Size = new System.Drawing.Size(205, 23);
+            this.buttonLedOn.TabIndex = 5;
+            this.buttonLedOn.Text = "LED On";
+            this.buttonLedOn.UseVisualStyleBackColor = true;
+            this.buttonLedOn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonLedOff
+            // 
+            this.buttonLedOff.Location = new System.Drawing.Point(52, 135);
+            this.buttonLedOff.Name = "buttonLedOff";
+            this.buttonLedOff.Size = new System.Drawing.Size(205, 23);
+            this.buttonLedOff.TabIndex = 6;
+            this.buttonLedOff.Text = "LED Off";
+            this.buttonLedOff.UseVisualStyleBackColor = true;
+            this.buttonLedOff.Click += new System.EventHandler(this.buttonLedOff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 211);
+            this.Controls.Add(this.buttonLedOff);
+            this.Controls.Add(this.buttonLedOn);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonLedOn;
+        private System.Windows.Forms.Button buttonLedOff;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
